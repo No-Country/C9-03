42eadc52242c3d6c2105530b8f35d7fa6cf870f2
Rocio Godoy <rgodoylarrosa@gmail.com>
2023-02-06 23:30:30 -0300

    Create user model, config devise in order to work with Turbo

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'static_pages#landing'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
 end
