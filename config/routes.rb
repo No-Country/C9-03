f446696a070e0fdfdb67260199dac9e8f624e675
Rocio Godoy <rgodoylarrosa@gmail.com>
2023-02-07 16:05:42 -0300

    Add name to User model, Devise controller and sign_up view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'static_pages#landing'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
 end
