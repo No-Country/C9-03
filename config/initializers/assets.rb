750f9b890072bf683de2f1dd1aa244166b61e691
Rocio Godoy <rgodoylarrosa@gmail.com>
2023-02-09 11:54:50 -0300

    Fix typo on file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,11 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-<<<<<<< HEAD
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
-=======
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
->>>>>>> 62cb154 (Fixed Dockerfile and docker-compose (development option), added boilerplate to test Bootstrap, added JS assets and configuration)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
